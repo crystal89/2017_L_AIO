033511f05ac405c3be934df3757a14e84fe4032c
crystal <liupingping@huaqin.com>
2017-07-10 10:57:58 +0800

    Revert "Revert "AIO - android工程 辅助u3d实现调用android系统的功能""
    This reverts commit af2784e4bfe232114d048923e4228362e49a16c4.

---
+++ mirageAIO/app/src/main/java/com/hq/mirageaio/PackageChangeInfo.java
@@ -0,0 +1,27 @@
+package com.hq.mirageaio;
+
+/**
+ * Created by Crystal on 2017/6/19.
+ */
+
+public class PackageChangeInfo {
+    private int flag;
+    private String name;
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
